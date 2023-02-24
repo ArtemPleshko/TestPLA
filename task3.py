eba810c4bcf34c54c0bfb04daf69a50043855f5b
ArtemPleshko <117635596+ArtemPleshko@users.noreply.github.com>
2023-02-24 16:41:30 +0200

    test

---
+++ task3.py
@@ -0,0 +1,14 @@
+import json
+#1
+names = "john peter brian Morgan Adam Maria bart"
+ans = names.title()
+print (ans)
+
+#4
+
+import re
+
+name = 'CamelCaseName'
+name = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+print( ["FirstItem", "FriendsList", "MyTuple"] )
+############
